ceb21fc69e9944b0f9c836fa762b55d5fdb80caa
NikitaZhukovsky <nikzuk2004@mail.ru>
2024-04-02 13:55:10 +0300

    Add management models

--- financial/management/admin.py
+++ financial/management/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from management.models import Category, Source, Transaction, Income, Balance
 
-# Register your models here.
+
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('source', 'amount', 'date_time')
+    search_fields = ('source', 'amount')
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('category', 'amount', 'date_time')
+    search_fields = ('category', 'amount')
+
+
+admin.site.register(Category)
+admin.site.register(Source)
+admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(Income, IncomeAdmin)
+admin.site.register(Balance)
+
--- financial/management/models.py
+++ financial/management/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Source(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "Sources"
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Transaction(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=20, decimal_places=2)
+    date_time = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length=150, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.amount} {self.date_time}"
+
+
+class Income(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    source = models.ForeignKey(Source, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=20, decimal_places=2)
+    date_time = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length=150, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.source} {self.amount} {self.date_time}"
+
+
+class Balance(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    current_balance = models.DecimalField(max_digits=20, decimal_places=2)
+
+    class Meta:
+        verbose_name_plural = "Balance"
+
+    def __str__(self):
+        return f"{self.current_balance}"
+
