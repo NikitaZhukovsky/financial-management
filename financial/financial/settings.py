2a585f2c5670511ffb3473dc0dd32bcf8e29fd25
NikitaZhukovsky <nikzuk2004@mail.ru>
2024-04-11 15:40:02 +0300

    Add swagger

--- financial/financial/settings.py
+++ financial/financial/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'financial.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -186,3 +186,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'api_key': {
+            'type': 'apiKey',
+            'description': 'Personal API key',
+            'name': 'Authorization',
+            'in': 'header',
+        }
+    }
+}
+
