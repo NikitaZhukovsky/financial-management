01a97d98b045200d5eb691bf4a7eb4612607e230
NikitaZhukovsky <nikzuk2004@mail.ru>
2024-04-14 12:02:54 +0300

    Add functionality for Sources

--- financial/management/urls.py
+++ financial/management/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from management.views import (CategoryListView, CategoryTransactionView, TransactionListView,
-                              BalanceView, IncomesListView, CustomCategoryView)
+                              BalanceView, IncomesListView, CustomCategoryView, SourceListView)
 
 urlpatterns = [
     path('balance/', BalanceView.as_view(), name='balance'),
     path('incomes/', IncomesListView.as_view(), name='incomes'),
+    path('sources/', SourceListView.as_view(), name='sources'),
     path('categories/', CategoryListView.as_view(), name='categories'),
     path('custom-categories/', CustomCategoryView.as_view(), name='custom_categories'),
     path('categories/<int:category_id>/', CategoryTransactionView.as_view(), name='category-transactions'),
